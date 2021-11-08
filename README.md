6c21bdff3f42abacc20dc4f5bbbbee988dde695b
Ankit Singh <singhakit25apr@gamil.com>
2021-11-08 18:23:09 +0530

    README ADDED

---
+++ README.md
@@ -0,0 +1,52 @@
+# Algorithm-Visualisations
+
+[Download](https://github.com/Anki-0/Algorithm-Visualisations)
+
+## Demonstration
+
+<div style="display:flex;flex-direction:row;gap:2rem;">
+
+<div>
+DEMO 1 (Selection Sort)
+
+![DEMO - IMG](Demonstration/Sorting%20Visualizer%20-%20Selection%20Sort.gif)
+
+</div>
+
+<div>
+DEMO 2 (Merge Sort)
+
+![DEMO - IMG](Demonstration/Sorting%20Visualizer%20-%20Merg%20Sort.gif)
+</div>
+
+</div>
+
+Visualization of different sorting algorithms in C++ with SDL2 Library.
+
+A sorting algorithm are the algorithums that used to sort the list or an array.There are multiple sorting algorithums out there, few are mention in project.
+
+#### To learn about SDL2 Check this Out: [SDL2 BASIC](sdltutorial.com/sdl-tutorial-basics)
+
+## Requirment
+
+- [C++](mingw-w64/org/downloads/)
+- [SDL2](libsdl.org/download-2.0.php) - Download
+
+## Installation
+
+[SDL2](lazyfoo.net/tutorial/SDL/01_hello_SDL/windows/index.php) - How to download and install SDL2 ?
+
+## Usage
+
+After Init your can use keyboard Event to select the menu items and press q to exit.
+
+Available Controls inside Sorting Visualizer:-
+
+- Use 0 to Generate a different randomized list.
+- Use 1 to start Selection Sort Algorithm.
+- Use 2 to start Insertion Sort Algorithm.
+- Use 3 to start Bubble Sort Algorithm.
+- Use 4 to start Merge Sort Algorithm.
+- Use 5 to start Quick Sort Algorithm.
+- Use 6 to start Heap Sort Algorithm.
+- Use q to exit out of Sorting Visualizer
--- main.cpp
+++ main.cpp
@@ -2,13 +2,11 @@
 #include <iostream>
 #include <string>
 
-
-
 void intro()
 {
     std::cout << "==============================Sorting Visualizer==============================\n\n"
-        << "Visualization of different sorting algorithms in C++ with SDL2 Library. A sorting algorithm is an algorithm that puts the elements of a list in a certain order. While there are a large number of sorting algorithms, in practical implementations a few algorithms predominate.\n"
-        << "Press ENTER to show controls...";
+              << "Visualization of different sorting algorithms in C++ with SDL2 Library. A sorting algorithm is an algorithm that puts the elements of a list in a certain order. While there are a large number of sorting algorithms, in practical implementations a few algorithms predominate.\n"
+              << "Press ENTER to show controls...";
 
     std::string s;
     getline(std::cin, s);
@@ -21,17 +19,17 @@ void intro()
 bool controls()
 {
     std::cout << "WARNING: Giving repetitive commands may cause latency and the visualizer may behave unexpectedly. Please give a new command only after the current command's execution is done.\n\n"
-        << "Available Controls inside Sorting Visualizer:-\n"
-        << "    Use 0 to Generate a different randomized list.\n"
-        << "    Use 1 to start Selection Sort Algorithm.\n"
-        << "    Use 2 to start Insertion Sort Algorithm.\n"
-        << "    Use 3 to start Bubble Sort Algorithm.\n"
-        << "    Use 4 to start Merge Sort Algorithm.\n"
-        << "    Use 5 to start Quick Sort Algorithm.\n"
-        << "    Use 6 to start Heap Sort Algorithm.\n"
-        << "    Use q to exit out of Sorting Visualizer\n\n"
-        << "PRESS ENTER TO START SORTING VISUALIZER...\n\n"
-        << "Or type -1 and press ENTER to quit the program.";
+              << "Available Controls inside Sorting Visualizer:-\n"
+              << "    Use 0 to Generate a different randomized list.\n"
+              << "    Use 1 to start Selection Sort Algorithm.\n"
+              << "    Use 2 to start Insertion Sort Algorithm.\n"
+              << "    Use 3 to start Bubble Sort Algorithm.\n"
+              << "    Use 4 to start Merge Sort Algorithm.\n"
+              << "    Use 5 to start Quick Sort Algorithm.\n"
+              << "    Use 6 to start Heap Sort Algorithm.\n"
+              << "    Use q to exit out of Sorting Visualizer\n\n"
+              << "PRESS ENTER TO START SORTING VISUALIZER...\n\n"
+              << "press ENTER to quit the program.";
     std::string S;
     getline(std::cin, S);
     if (S == "")
@@ -45,14 +43,16 @@ bool controls()
     return true;
 }
 
-int main(int arg,char* args[]) {
-	CApp thisApp;
+int main(int arg, char *args[])
+{
+    CApp thisApp;
 
     intro();
     while (1)
     {
         std::cout << '\n';
-        if (controls()) {
+        if (controls())
+        {
             thisApp.onExecute();
         }
         else
@@ -62,6 +62,5 @@ int main(int arg,char* args[]) {
         }
     }
 
-	
-	return 0;
+    return 0;
 }
